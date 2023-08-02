148482120b06a237740f9bd20a4768758178ee99
Marley <marleyburrows1@gmail.com>
2023-08-01 20:50:36 -0700

    fin

--- cashflow-ui/src/components/ProfileView/Badges.jsx
+++ cashflow-ui/src/components/ProfileView/Badges.jsx
@@ -1,4 +1,4 @@
-import { Heading, Image, useColorModeValue, Flex } from "@chakra-ui/react";
+import { Heading, Image, useColorModeValue, Flex, Box } from "@chakra-ui/react";
 import { useState } from "react";
 
 export default function Badges({ appState }) {
@@ -7,22 +7,22 @@ export default function Badges({ appState }) {
   function determineBadges(userPoints) {
     let badges = [];
     if (userPoints >= 500) {
-      badges.push("cash-cadet.png");
+      badges.push("cash-cadet");
     }
     if (userPoints >= 1000) {
-      badges.push("dolla-scholar.png");
+      badges.push("dolla-scholar");
     }
     if (userPoints > 1600) {
-      badges.push("money-maverick.png");
+      badges.push("money-maverick");
     }
     if (userPoints > 2000) {
-      badges.push("profit-prodigy.png");
+      badges.push("profit-prodigy");
     }
     if (userPoints > 3000) {
-      badges.push("wealth-wizard.png");
+      badges.push("wealth-wizard");
     }
     if (userPoints > 3600) {
-      badges.push("cashflow-champion.png");
+      badges.push("cashflow-champion");
     }
     return badges;
   }
@@ -43,10 +43,56 @@ export default function Badges({ appState }) {
       <Flex justifyContent={'center'} >
       {earnedBadges.map((badge) => {
         return (
-          <Image maxW={'200px'} key={badge} src={badge} alt="Badge" />
+            <Box className="flip"
+            css={{
+                //transform: 'translateX(-50%)',
+                width: '300px',
+                height: '300px',
+                '&:hover': {
+                  '.flip-inner': {
+                    transform: 'rotateY(180deg)',
+                  },
+                },
+                '.flip-inner': {
+                  transformStyle: 'preserve-3d',
+                  transition: 'transform .8s ease',
+                  height: '100%',
+                  width: '100%',
+                  '.front, .back': {
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    backfaceVisibility: 'hidden',
+                    borderRadius: '50%',
+                    overflow: 'hidden',
+                  },
+                  '.front': {
+                    transform: 'translateZ(1px)',
+                  },
+                  '.back': {
+                    transform: 'rotateY(180deg)',
+                  },
+                  '.thumbnail': {
+                    width: '100%',
+                  },
+                },
+              }}>
+            <Box className="flip-inner">
+                <Box className="front">
+                <Image maxW={'300px'} key={badge} src={`${badge}.png`} alt={badge} />
+                </Box>
+                <Box className="back">
+                <Image maxW={'300px'} key={badge} src={`${badge}2.png`} alt={badge} />
+                </Box>
+            </Box>
+        </Box>
         );
       })}
       </Flex>
     </>
   );
 }
+
+
